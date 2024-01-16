9233529f9012b7c1cbd13722df6de78b470fb84b
Viral Jain <viral.jain@sigmainfo.net>
2024-01-16 20:47:46 +0530

    css changes and user created

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,14 +1,24 @@
 class Loan < ApplicationRecord
     validates :name, presence: true, length: { minimum: 4, maximum: 100 }
     validates :email, presence: true, length: { minimum: 4, maximum: 100 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :middle_name, length: { maximum: 100 } # Allow blank, but validate length if present
+    validates :last_name, length: { maximum: 100 } # Allow blank, but validate length if present
+    validates :date_of_birth, presence: true
     validates :amount, presence: true, numericality: { greater_than: 0 }
-    validates :purpose, presence: true, length: { minimum: 4, maximum: 50 }
-    validates :ssn, presence: true, length: {  minimum: 5, maximum: 9 }
-    validates :phone_number, presence: true, length: {  minimum: 5, maximum: 10}
+    validates :purpose, presence: true, inclusion: { in: %w(Home Car Education Business Wedding) }, length: { minimum: 4, maximum: 50 }
+    validates :ssn, presence: true
+    validates :phone_number, presence: true
     validates :address, presence: true, length: { minimum: 4, maximum: 100 }
-    validates :income, presence: true, numericality: { greater_than: 0 }
-    validates :profession, presence: true, length: { minimum: 4, maximum: 100 }
-
+    validates :home_number, numericality: { only_integer: true }, allow_blank: true
+    validates :street_address2, length: { maximum: 100 }, allow_blank: true
+    validates :city, length: { maximum: 50 }, allow_blank: true
+    validates :state, length: { maximum: 50 }, allow_blank: true
+    validates :zip, numericality: { only_integer: true }, allow_blank: true
+    validates :residence_type, length: { maximum: 50 }, allow_blank: true
+    validates :annual_income, presence: true, numericality: { greater_than: 0 }
+    validates :employer_name, length: { maximum: 100 }, allow_blank: true
+    validates :employer_phone, numericality: { only_integer: true }, allow_blank: true
+  
     enum purpose: [:Home, :Car, :Education, :Business, :Wedding]
-
-end
+  end
+  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_08_093911) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_151151) do
   create_table "loans", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -32,6 +32,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_08_093911) do
     t.decimal "annual_income"
     t.string "employer_name"
     t.decimal "employer_phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
