734367d2b91da7113aa6585390ba7c0e92e69b25
Viral Jain <viral.jain@sigmainfo.net>
2024-01-16 21:15:56 +0530

    authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_16_152149) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_154013) do
   create_table "loans", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_16_152149) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
