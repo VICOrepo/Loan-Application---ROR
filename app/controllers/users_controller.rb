cb9d53c352d4a80126ab1373cc4b590b05ec1044
Viral Jain <viral.jain@sigmainfo.net>
2024-01-16 22:41:49 +0530

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+        @user = User.find(params[:id])
+      end
+    
+      def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          flash[:notice] = "Your account information was successfully updated"
+          redirect_to new_loan_path
+        else
+           redirect_to edit_user_path, alert: @user.errors.full_messages.join(", ")    
+        end
+    end
+
   def create
     @user = User.new(user_params)
     if @user.save
